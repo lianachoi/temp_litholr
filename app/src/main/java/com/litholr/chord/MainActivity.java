4b04de2dee7001dfb9978b3c61e0828f9b0a635b
lianachoi <2ndsunmail@gmail.com>
2021-04-05 04:27:29 +0900

    20210405-04:25 음성 녹음 및 재생 기능 구현                + 카카오톡 연동 로그인 기능 구현                  (*아직 해결할 문제가 많음)

--- app/src/main/java/com/litholr/chord/MainActivity.java
+++ app/src/main/java/com/litholr/chord/MainActivity.java
@@ -1,17 +1,34 @@
 package com.litholr.chord;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.kakao.auth.AuthType;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.usermgmt.response.model.Profile;
+import com.kakao.usermgmt.response.model.UserAccount;
+import com.kakao.util.OptionalBoolean;
+import com.kakao.util.exception.KakaoException;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -26,6 +43,10 @@ public class MainActivity extends AppCompatActivity {
 
     final int REQUEST_PERMISSION_CODE = 1000;
 
+    private Button btn_custom_login;
+    private Button btn_custom_login_out;
+    private SessionCallback sessionCallback = new SessionCallback();
+    Session session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,31 +56,40 @@ public class MainActivity extends AppCompatActivity {
         if (!checkPermissionFromDevice())
             requestPermission();
 
-        btnStartPlay = (Button)findViewById(R.id.btnStartPlay);
-        btnStartRecord = (Button)findViewById(R.id.btnStartRecord);
-        btnStopPlay = (Button)findViewById(R.id.btnStopPlay);
-        btnStopRecord = (Button)findViewById(R.id.btnStopRecord);
+        btnStartPlay = (Button) findViewById(R.id.btnStartPlay);
+        btnStartRecord = (Button) findViewById(R.id.btnStartRecord);
+        btnStopPlay = (Button) findViewById(R.id.btnStopPlay);
+        btnStopRecord = (Button) findViewById(R.id.btnStopRecord);
 
+        btn_custom_login = (Button) findViewById(R.id.btn_custom_login);
+        btn_custom_login_out = (Button) findViewById(R.id.btn_custom_login_out);
 
+        session = Session.getCurrentSession();
 
-        btnStartRecord.setOnClickListener(new View.OnClickListener(){
+//        Intent intent = new Intent(this, SessionCallback.class);
+//        intent.putExtra("nickname", "");
+//        startActivity(intent);
+
+        session.addCallback(sessionCallback);
+
+        btnStartRecord.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                    pathSave = getFilesDir()+"/"
-                            + UUID.randomUUID().toString()+"_audio_record.3gp";
-                    setupMediaRecorder();
-                    try{
-                        mediaRecorder.prepare();
-                        mediaRecorder.start();
-                    }catch (IOException e) {
-                        e.printStackTrace();
-                    }
+            public void onClick(View view) {
+                pathSave = getFilesDir() + "/"
+                        + UUID.randomUUID().toString() + "_audio_record.3gp";
+                setupMediaRecorder();
+                try {
+                    mediaRecorder.prepare();
+                    mediaRecorder.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-                    btnStartPlay.setEnabled(false);
-                    btnStopPlay.setEnabled(false);
-                    btnStopRecord.setEnabled(true);
+                btnStartPlay.setEnabled(false);
+                btnStopPlay.setEnabled(false);
+                btnStopRecord.setEnabled(true);
 
-                    Toast.makeText(MainActivity.this,"Recording...", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Recording...", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -92,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 mediaPlayer.start();
-                Toast.makeText(MainActivity.this,"Playing...",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Playing...", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -104,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
                 btnStopPlay.setEnabled(false);
                 btnStartPlay.setEnabled(true);
 
-                if (mediaPlayer != null){
+                if (mediaPlayer != null) {
                     mediaPlayer.stop();
                     mediaPlayer.release();
                     setupMediaRecorder();
@@ -112,6 +142,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn_custom_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                session.open(AuthType.KAKAO_LOGIN_ALL, MainActivity.this);
+
+            }
+        });
+
+        btn_custom_login_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+                UserManagement.getInstance()
+                        .requestLogout(new LogoutResponseCallback() {
+
+                            @Override
+                            public void onCompleteLogout() {
+                                Toast.makeText(MainActivity.this, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
     }
 
     private void setupMediaRecorder() {
@@ -123,36 +175,120 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private boolean checkPermissionFromDevice(){
+    private boolean checkPermissionFromDevice() {
         int write_external_storage_result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
         int record_audio_result = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
-        int read_external_storage_result = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
 
         return write_external_storage_result == PackageManager.PERMISSION_GRANTED &&
-                read_external_storage_result == PackageManager.PERMISSION_GRANTED &&
                 record_audio_result == PackageManager.PERMISSION_GRANTED;
     }
 
 
     private void requestPermission() {
-        ActivityCompat.requestPermissions(this,new String[]{
+        ActivityCompat.requestPermissions(this, new String[]{
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.RECORD_AUDIO
 
-        },REQUEST_PERMISSION_CODE);
+        }, REQUEST_PERMISSION_CODE);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
-            case REQUEST_PERMISSION_CODE:
-            {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_CODE: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                     Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
                 else
-                    Toast.makeText(this,"Permission Denied", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
             }
         }
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 세션 콜백 삭제
+        Session.getCurrentSession().removeCallback(sessionCallback);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        // 카카오톡|스토리 간편로그인 실행 결과를 받아서 SDK로 전달
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    public class SessionCallback implements ISessionCallback {
+
+        @Override
+        public void onSessionOpened() {
+            requestMe();
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            Log.e("SessionCallback :: ", "onSessionOpenFailed : " + exception.getMessage());
+        }
+
+        private void requestMe() {
+            UserManagement.getInstance()
+                    .me(new MeV2ResponseCallback() {
+                        @Override
+                        public void onSessionClosed(ErrorResult errorResult) {
+                            Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
+                        }
+
+                        @Override
+                        public void onFailure(ErrorResult errorResult) {
+                            Log.e("KAKAO_API", "사용자 정보 요청 실패: " + errorResult);
+                        }
+
+                        @Override
+                        public void onSuccess(MeV2Response result) {
+                            Log.i("KAKAO_API", "사용자 아이디: " + result.getId());
+
+                            UserAccount kakaoAccount = result.getKakaoAccount();
+                            if (kakaoAccount != null) {
+
+                                // 이메일
+                                String email = kakaoAccount.getEmail();
+
+                                if (email != null) {
+                                    Log.i("KAKAO_API", "email: " + email);
+
+                                } else if (kakaoAccount.emailNeedsAgreement() == OptionalBoolean.TRUE) {
+                                    // 동의 요청 후 이메일 획득 가능
+                                    // 단, 선택 동의로 설정되어 있다면 서비스 이용 시나리오 상에서 반드시 필요한 경우에만 요청해야 합니다.
+
+                                } else {
+                                    // 이메일 획득 불가
+                                }
+
+                                // 프로필
+                                Profile profile = kakaoAccount.getProfile();
+
+                                if (profile != null) {
+                                    Log.d("KAKAO_API", "nickname: " + profile.getNickname());
+                                    Log.d("KAKAO_API", "profile image: " + profile.getProfileImageUrl());
+                                    Log.d("KAKAO_API", "thumbnail image: " + profile.getThumbnailImageUrl());
+                                    Toast.makeText(MainActivity.this,  profile.getNickname() + "로그인 되었습니다.", Toast.LENGTH_SHORT).show();
+
+                                } else if (kakaoAccount.profileNeedsAgreement() == OptionalBoolean.TRUE) {
+                                    // 동의 요청 후 프로필 정보 획득 가능
+
+                                } else {
+                                    // 프로필 획득 불가
+                                }
+                            }
+                        }
+                    });
+
+        }
+    }
 }
+
